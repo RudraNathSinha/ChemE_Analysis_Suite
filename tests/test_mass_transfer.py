05eb8d10ddffee323a30a56fdc0d577655bad299
RudraNathSinha <rudranathsinha7@gmail.com>
2025-03-28 16:18:44 +0530

    Create test_mass_transfer.py

---
+++ tests/test_mass_transfer.py
@@ -0,0 +1,7 @@
+import unittest
+from app.utils import mass_transfer_calc
+
+class TestMassTransfer(unittest.TestCase):
+    def test_mass_transfer_calculation(self):
+        # Add test cases
+        pass
